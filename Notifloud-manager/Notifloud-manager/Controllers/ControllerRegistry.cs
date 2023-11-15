c35e068530404817a955d7e6a38224698702ff33
unknown <yrezehi@gmail.com>
2023-11-15 07:19:01 -0800

    init

---
+++ Notifloud-manager/Notifloud-manager/Controllers/ControllerRegistry.cs
@@ -0,0 +1,19 @@
+﻿using UI.HTML;
+
+namespace Notifloud_manager.Controllers
+{
+    public static class ControllerRegistry
+    {
+        public static void RegisterControllers(this WebApplication application)
+        {
+            application.Index();
+            application.Assets();
+        }
+
+        private static void Index(this WebApplication application) =>
+            application.MapGet("/", () => Results.Extensions.Serve("index.html"));
+
+        private static void Assets(this WebApplication application) =>
+            application.MapGet("/Assets/{*asset}", (string asset) => Results.Extensions.Serve("Assets\\" + asset));
+    }
+}
