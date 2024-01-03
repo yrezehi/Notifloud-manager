cceb9a5492037918514f05c553f43785924adf36
unknown <yrezehi@gmail.com>
2024-01-03 08:45:17 -0800

    add: NotificationQueue

--- Notifloud-manager/Core/Services/Queue/Notification.cs
+++ Notifloud-manager/Core/Services/Queue/Notification.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Services.Queue.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,9 @@ namespace Core.Services.Queue
 {
     public class Notification
     {
+        public string Title { get; set; }
+        public string Message { get; set; }
+
         public int TTL { get; set; }
         public NotificationPriority Priority { get; set; }
     }
--- Notifloud-manager/Core/Services/Queue/NotificationQueue.cs
+++ Notifloud-manager/Core/Services/Queue/NotificationQueue.cs
@@ -4,19 +4,30 @@ namespace Core.Services.Queue
 {
     public class NotificationQueue
     {
-        private readonly ConcurrentQueue<string> Notifications;
+        private readonly ConcurrentQueue<Notification> Notifications;
+        private readonly SemaphoreSlim Signal = new SemaphoreSlim(0);
 
         public NotificationQueue() =>
-            (Notifications) = (new ConcurrentQueue<string>());
+            (Notifications, Signal) = (new ConcurrentQueue<Notification>(), new SemaphoreSlim(0));
        
-        public void Enqueue()
+        public void Enqueue(Notification notification)
         {
-
+            Notifications.Enqueue(notification);
+            Signal.Release();
         }
 
-        public void Dequeue()
+        public async Task<Notification> Dequeue(CancellationToken cancellationToken)
         {
+            await Signal.WaitAsync(cancellationToken);
 
+            Notifications.TryDequeue(out Notification? notification);
+
+            if(notification == null)
+            {
+                throw new MemberAccessException("No notification left!");
+            }
+
+            return notification;
         }
     }
 }
