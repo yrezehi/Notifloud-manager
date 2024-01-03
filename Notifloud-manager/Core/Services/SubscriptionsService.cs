b4ad3bf8ed469fdbd7d547a1923cc72091f7f7d0
unknown <yrezehi@gmail.com>
2024-01-03 09:10:36 -0800

    add: Notifications

--- Notifloud-manager/Core/Services/SubscriptionsService.cs
+++ Notifloud-manager/Core/Services/SubscriptionsService.cs
@@ -1,19 +1,19 @@
 ﻿using Core.Models;
 using Core.Repositories.Abstracts.Interfaces;
 using Core.Services.Abstract;
+using Core.Services.Queue;
 
 namespace Core.Services
 {
     public class SubscriptionsService : ServiceBase<Subscription>
     {
         public readonly string PublicKey { get; set; }
+        public readonly NotificationQueue Notifications { get; set; }
 
-        public SubscriptionsService(IUnitOfWork unitOfWork) : base(unitOfWork) =>
-            PublicKey = "X83GLEFW933";
+        public SubscriptionsService(IUnitOfWork unitOfWork, NotificationQueue notifications) : base(unitOfWork) =>
+            (Notifications, PublicKey) = (notifications, "X83GLEFW933");
            
-        public void Send(string content)
-        {
-            
-        }
+        public void Enqueue(Notification notification) =>
+            Notifications.Enqueue(notification);
     }
 }
--- Notifloud-manager/UI/Controllers/ControllerRegistry.cs
+++ Notifloud-manager/UI/Controllers/ControllerRegistry.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Models;
 using Core.Services;
+using Core.Services.Queue;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Notifloud_manager.HTML;
@@ -19,6 +20,8 @@ namespace Notifloud_manager.Controllers
 
             application.Subscribe();
             application.Subscriptions();
+
+            application.Notification();
         }
 
         private static void Index(this WebApplication application) =>
@@ -28,9 +31,12 @@ namespace Notifloud_manager.Controllers
             application.MapGet("/Assets/{*asset}", (string asset) => Results.Extensions.Serve("Assets\\" + asset));
 
         private static void Subscribe(this WebApplication application) =>
-            application.MapPost("/Subscriptions/Subscribe", async ([FromBody] Subscription subscription, SubscriptionsService service) => await service.Create(subscription));
+            application.MapPost("/Subscriptions", async ([FromBody] Subscription subscription, SubscriptionsService service) => await service.Create(subscription));
 
         private static void Subscriptions(this WebApplication application) =>
-           application.MapPost("/Subscriptions", async ([FromBody] Subscription subscription, SubscriptionsService service) => await service.GetAll());
+           application.MapGet("/Subscriptions", async (SubscriptionsService service) => await service.GetAll());
+
+        private static void Notification(this WebApplication application) =>
+            application.MapPost("/Notifications", ([FromBody] Notification notification, SubscriptionsService service) => service.Enqueue(notification));
     }
 }
