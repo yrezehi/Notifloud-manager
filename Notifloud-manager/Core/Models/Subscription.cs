52c71bb9b5708d58ff3286fb085ff0dc72e15ec9
unknown <yrezehi@gmail.com>
2024-01-03 05:22:54 -0800

    set: SubscriptionsService

--- Notifloud-manager/Core/Models/Subscription.cs
+++ Notifloud-manager/Core/Models/Subscription.cs
@@ -4,5 +4,11 @@
     {
         public string Endpoint { get; set; }
         public IDictionary<string, string> Keys { get; set; }
+
+        public Subscription(string endpoint, IDictionary<string, string> keys) =>
+            (Endpoint, Keys) = (endpoint, keys);
+
+        public static Subscription Create(string endpoint, IDictionary<string, string> keys) =>
+            new Subscription(endpoint, keys);
     }
 }
