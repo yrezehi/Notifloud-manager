2b92fdbd89d78e8e3e3d5711e90f1296c3706fff
unknown <yrezehi@gmail.com>
2024-01-03 04:49:37 -0800

    setup: sub model

---
+++ Notifloud-manager/Core/Models/Subscription.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Models
+{
+    public class Subscription
+    {
+        public string Endpoint { get; set; }
+        public IDictionary<string, string> Keys { get; set; }
+    }
+}
